db1bfe548b30a36d93dc741af09674b9252f8b0f
ysadka <ysadka@gmail.com>
2013-06-05 11:07:15 -0700

    added factories file

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,10 +4,21 @@ require 'spec_helper'
 describe 'User' do
   let(:user) {User.new(username: "Me", password: "1234", email: "email@email.com")}
 
+  FactoryGirl.define do
+    factory :user do
+      username "Me"
+      password "1234"
+      email    "email@email.com"
+      admin false
+    end
+  end
+
   context "sign-up" do
     it "should have username"
     it "should have password"
     it "should have email"
   end
+
+
 
 end
