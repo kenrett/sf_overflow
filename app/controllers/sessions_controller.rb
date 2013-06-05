54dcc41521baf41b13956e2e4020cf6b17cc5352
nathanielwroblewski <nathanielwroblewski@gmail.com>
2013-06-05 16:20:59 -0700

    fixed stuff

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,17 @@
 class SessionsController < ApplicationController
-authenticated new users
+# authenticated new users
 
   def create
-    p params
-    @user = User.find_by_email!(params["email"])
+    @user = User.find_by_email(params["email"])
     if @user.password = params["password"]
       session[:id] = @user.id
+      p "good"
       render :nothing => true, status: :ok
     else
+      p "bad"
       render :nothing => true, status: :bad_request
     end
   end
+
 
 end
