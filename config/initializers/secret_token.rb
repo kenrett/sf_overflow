443b48800a145041ae08079df877355d9daa80ce
fmackojc <fab.mackojc@gmail.com>
2013-06-05 09:48:23 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SfOverflow::Application.config.secret_token = 'd661d5f314fede430d0d58cf3f5debd23aeb615f64221f3eeadde5081c4e786281af2bade0210a73f5f99d5af942f15a6015530bbc26aa861038147c47c035a5'
