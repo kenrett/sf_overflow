8366b15a88975b45972d1c6ba046cc94d186dac8
ysadka <ysadka@gmail.com>
2013-06-05 11:44:40 -0700

    added factories file

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,1 +1,21 @@
-factories.rb
+FactoryGirl.define do
+  factory :user do
+    username "Me"
+    password "1234"
+    email    "email@email.com"
+    admin false
+  end
+
+   factory :comment do
+    title "Moving to the bay"
+    description "Blah blah blah"
+    user
+  end
+
+   factory :question do
+    name "Foobar question"
+    description "Blah blah blah"
+    user
+  end
+end
+
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,17 +2,8 @@ require 'spec_helper'
 
 
 describe 'User' do
-  let(:user) {User.new(username: "Me", password: "1234", email: "email@email.com")}
-
-  FactoryGirl.define do
-    factory :user do
-      username "Me"
-      password "1234"
-      email    "email@email.com"
-      admin false
-    end
-  end
-
+  Factory.create(:user)
+  
   context "sign-up" do
     it "should have username"
     it "should have password"
