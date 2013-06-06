281685d831c58d3ed2f2092cb8fd365beaf36cb0
ysadka <ysadka@gmail.com>
2013-06-06 09:28:58 -0700

    spec file structure update

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -9,9 +9,9 @@ describe 'User' do
       it "should route to profile page" do
         visit root_path
         click_link('Signup')
-        fill_in('user_username', with: "test")
-        fill_in('user_email', with: "test@email.com")
-        fill_in('user_password', with: "1234")
+        fill_in('user_username', :with => "test")
+        fill_in('user_email', :with => "test@email.com")
+        fill_in('user_password', :with => "1234")
         click_button('Create User')
         current_path.should == user_path(user)
       end
@@ -21,9 +21,9 @@ describe 'User' do
       it "should return an error if the username or email is not unique" do
         visit root_path
         click_link('Signup')
-        fill_in('user_username', with: "test")
-        fill_in('user_email', with: "test@email.com")
-        fill_in('user_password', with: "1234")
+        fill_in('user_username', :with => user.username)
+        fill_in('user_email', :with => user.email)
+        fill_in('user_password', :with => "1234")
         click_button('Create User')
         current_path.should == user_path(user)
       end
@@ -37,8 +37,8 @@ describe 'User' do
     context "successful sign-in" do
       it "should route to profile page" do
         visit root_path
-        fill_in('email', with: user.email)
-        fill_in('password', with: user.password)
+        fill_in('email', :with => user.email)
+        fill_in('password', :with => user.password)
         click_button('Submit')
         current_path.should == user_path(user)
       end
