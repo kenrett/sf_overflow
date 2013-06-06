86fa57014fd3f09fb73cd7a0c3867ea39419d481
Amy Lukima <amylukima@gmail.com>
2013-06-06 09:48:43 -0700

    correct Factory Girl create user

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,31 +1,32 @@
+require 'faker'
+
 FactoryGirl.define do
   factory :user, :aliases => [:commenter] do
-    sequence(:username) { |n| "Me#{n}"}
-    password            "1234"
-    sequence(:email)    { "#{username}@#{username}.com" }
-    admin false
-
-
+    sequence(:username) { |n| Faker::Name.name }
+    password            "password"
+    sequence(:email)    { "#{SecureRandom.uuid}@example.com" }
     factory :admin do
       admin true
     end
   end
 
   factory :comment do
-    title "Moving to the bay"
-    description "Blah blah blah"
+    title { Faker::Lorem.sentence }
+    description { Faker::Lorem.paragraph }
     author
   end
 
   factory :question do
-    description "Blah blah blah"
-    commenter
+    title { Faker::Lorem.sentence }
+    description { Faker::Lorem.paragraph }
+    user
   end
 
   factory :answer do
+
     description { Faker::Lorem.paragraph }
-    user
-    question
+    association :answerable, :factory => :question 
+    association :user
   end
 end
 
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -2,88 +2,51 @@ require 'spec_helper'
 
 describe Answer do
   let(:description) {"hello"}
-  let(:answer) { FactoryGirl.create(:user) }
-  let(:user) { answer.user }
+  let(:answer) { FactoryGirl.create(:answer) }
+  # let(:user) { answer.user }
+  
   context "#initialize" do
-
-    describe "invalid input" do
-      it ""
-      it ""
-    end
-    describe "vali"
-  end
-  context 'reply to question'
-
-  it "should have a parent answer id"
-  it "should have a description" 
-
-  describe ".children"
-  it "should display child posts"
-
-  context "when logged in"
-  it "should redirect to login page"
-
-  context "when logged out"
-  it "should post the comment"
-
-  # describe "GET /answer" do
-  #   it "displays answer" do
-  #     Answer.create!(:description "hello there!")
-  #     visit task_path
-  #     response.body.should include("hello there!")
-  #   end
-  # end
-
-
-
-# let(:account) {Account.new(acct_number, starting_balance)}
-#   let(:acct_number) { '1123456789' }
-#   let(:starting_balance) { 100 }
-
-  describe "#initialize" do
 
     context "with valid input" do
       it "creates a question with user_id" do
         answer.user_id.should eq(user_id)
       end
-
-      it 
-    end
-
-    
-  end
-  context "user signup" do
-    desribe "valid input" do
-      it "errors show on the sinpup page" do
-        pending
-      end
-    end
-    describe "invalid input" do
-      it "redirect user to the"
     end
   end
-  describe "POST /answer" do
-    it "creates a answer" do
-      post_via_redirect question_path, :answer => { :description => "mow lawn" }
-      response.body.should include("mow lawn")
-      #need to go through form! Use capybara
-    end
-  end 
 
-  describe '#destroy' do
 
-    context 'when answer is found' do
-      it 'responds with 200'
-      it 'shows the answer'
-    end
+#   context "user signup" do
+#     desribe "valid input" do
+#       it "errors show on the sinpup page" do
+#         pending
+#       end
+#     end
+#     describe "invalid input" do
+#       it "redirect user to the"
+#     end
+#   end
+#   describe "POST /answer" do
+#     it "creates a answer" do
+#       post_via_redirect question_path, :answer => { :description => "mow lawn" }
+#       response.body.should include("mow lawn")
+#       #need to go through form! Use capybara
+#     end
+#   end 
 
-    context 'when answer is not found' do
-      it 'responds with 404'
-    end
+#   describe '#destroy' do
 
-    context 'when answer is not owned' do
-      it 'responds with 404'
-    end
-  end
-#database_cleaner
+#     context 'when answer is found' do
+#       it 'responds with 200'
+#       it 'shows the answer'
+#     end
+
+#     context 'when answer is not found' do
+#       it 'responds with 404'
+#     end
+
+#     context 'when answer is not owned' do
+#       it 'responds with 404'
+#     end
+#   end
+# #database_cleaner
 end
